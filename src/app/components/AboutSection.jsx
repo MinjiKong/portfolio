d044b79499c210877b51d178f3d35e063136c495
MinjiKong <eoeomj97@gmail.com>
2024-05-01 22:50:34 -0700

    added New York Times Best Sellers project & typescript skill

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -10,18 +10,71 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="grid grid-cols-5 gap-5">
-        <Skill imageSrc="images/skills/java.png" skillName="Java" percentage="90%" />
-        <Skill imageSrc="images/skills/python.png" skillName="Python" percentage="90%" />
-        <Skill imageSrc="images/skills/dart.png" skillName="Dart" percentage="60%" />
-        <Skill imageSrc="images/skills/kotlin.png" skillName="Kotlin" percentage="60%" />
-        <Skill imageSrc="images/skills/flutter.png" skillName="Flutter" percentage="80%" />
-        <Skill imageSrc="images/skills/git.png" skillName="Git" percentage="90%" />
-        <Skill imageSrc="images/skills/react.png" skillName="React" percentage="70%" />
-        <Skill imageSrc="images/skills/nextjs.png" skillName="Next.js" percentage="50%" />
-        <Skill imageSrc="images/skills/javascript.png" skillName="JavaScript" percentage="80%" />
-        <Skill imageSrc="images/skills/tailwind.png" skillName="Tailwind" percentage="50%" />
-        <Skill imageSrc="images/skills/aws.png" skillName="AWS" percentage="50%" />
-        <Skill imageSrc="images/skills/firebase.png" skillName="Firebase" percentage="50%" />
+        <Skill
+          imageSrc="images/skills/java.png"
+          skillName="Java"
+          percentage="60%"
+        />
+        <Skill
+          imageSrc="images/skills/python.png"
+          skillName="Python"
+          percentage="60%"
+        />
+        <Skill
+          imageSrc="images/skills/dart.png"
+          skillName="Dart"
+          percentage="60%"
+        />
+        <Skill
+          imageSrc="images/skills/kotlin.png"
+          skillName="Kotlin"
+          percentage="60%"
+        />
+        <Skill
+          imageSrc="images/skills/flutter.png"
+          skillName="Flutter"
+          percentage="80%"
+        />
+        <Skill
+          imageSrc="images/skills/git.png"
+          skillName="Git"
+          percentage="90%"
+        />
+        <Skill
+          imageSrc="images/skills/react.png"
+          skillName="React"
+          percentage="70%"
+        />
+        <Skill
+          imageSrc="images/skills/nextjs.png"
+          skillName="Next.js"
+          percentage="70%"
+        />
+        <Skill
+          imageSrc="images/skills/javascript.png"
+          skillName="JavaScript"
+          percentage="80%"
+        />
+        <Skill
+          imageSrc="images/skills/typescript.png"
+          skillName="TypeScript"
+          percentage="50%"
+        />
+        <Skill
+          imageSrc="images/skills/tailwind.png"
+          skillName="Tailwind"
+          percentage="60%"
+        />
+        <Skill
+          imageSrc="images/skills/aws.png"
+          skillName="AWS"
+          percentage="50%"
+        />
+        <Skill
+          imageSrc="images/skills/firebase.png"
+          skillName="Firebase"
+          percentage="50%"
+        />
       </ul>
     ),
   },
@@ -30,7 +83,10 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>British Columbia of institute of Technology - Computer Systems Technology Diploma</li>
+        <li>
+          British Columbia of institute of Technology - Computer Systems
+          Technology Diploma
+        </li>
         <li>Douglas College - Hospitality Management Diploma</li>
       </ul>
     ),
@@ -64,11 +120,14 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          I am a software developer, driven by a deep passion for crafting interactive and responsive web and mobile applications. 
-          My primary expertise is in front-end development, where I combine creativity with 
-          meticulous attention to detail to ensure the creation of user-friendly applications that captivate and delight.
-          As a quick learner, I am always on the lookout for new skills and technologies.
-          I am looking forward to adventure this dynamic and ever-evolving field!
+            I am a software developer, driven by a deep passion for crafting
+            interactive and responsive web and mobile applications. My primary
+            expertise is in front-end development, where I combine creativity
+            with meticulous attention to detail to ensure the creation of
+            user-friendly applications that captivate and delight. As a quick
+            learner, I am always on the lookout for new skills and technologies.
+            I am looking forward to adventure this dynamic and ever-evolving
+            field!
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
